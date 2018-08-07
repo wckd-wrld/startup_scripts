f5b2d0612f71fa73eed1379f864781b555c69505
Jack Bennett <franc-bennett@protonmail.com>
2018-08-08 03:17:16 +1000

    Menu Drawing
    Menu is drawing and keyboard input works but needs tuning.

---
+++ workspace_select.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/ruby
+
+require "curses"
+
+class Menu < Curses::Window
+    @menuItems
+    @selectedItem
+    
+    def initialize(menuItems)
+        super(Curses.lines / 2 - 1, Curses.cols / 2 - 1, 0, 0)
+        @menuItems = Array.new(menuItems)
+        @selectedItem = @menuItems[0]
+        return self
+    end
+    
+    def start
+        self.draw
+        self.inputrouter
+    end
+
+    def inputrouter
+        while true
+            index = @menuItems.find_index(@selectedItem)
+            case self.getch
+            when 'w'
+                @selectedItem = @menuItems[index-1]
+                self.draw
+            when 'a' 
+                @selectedItem = @menuItems[index+1]
+                self.draw
+            when Curses::Key::ENTER
+            end
+        end
+    end 
+        
+    def draw
+        self.clear
+        for item in @menuItems
+            if @selectedItem == item
+                self.addstr("> " + item + "\n")
+            else
+                self.addstr("  " + item + "\n") 
+            end
+        end
+        self.refresh
+    end
+    public :initialize, :start
+end 
+
+
+
+Curses.init_screen
+begin
+    menu = Menu.new(['Web-Dev', 'Security', 'Game-Dev'])
+    menu.start
+ensure
+	Curses.close_screen
+    
+end
